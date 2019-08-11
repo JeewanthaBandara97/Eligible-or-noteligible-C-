a22837e8e71b06ac16b210c9c10fa0e88b77c399
Jeewantha Bandara <51124088+JeewanthaBandara97@users.noreply.github.com>
2019-08-11 17:54:21 +0530

    Add files via upload

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter Your Age :");
+            age = int.Parse(Console.ReadLine());
+
+            if (age>=18)
+            {
+                Console.WriteLine("eligible");
+            }
+            else
+            {
+                Console.WriteLine("Not eligible");
+            }
+            Console.ReadKey();
+        }
+    }
+}
